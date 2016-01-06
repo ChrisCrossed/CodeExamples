73bfc74e85662afcf72f55f6096729e903f11151
Chris Christensen <unclesam098@gmail.com>
2016-01-05 23:29:32 -0800

    fine

--- TournamentCaster/TournamentCaster/Assets/Cs_SystemManager.cs
+++ TournamentCaster/TournamentCaster/Assets/Cs_SystemManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Cs_SystemManager : MonoBehaviour
 {
@@ -9,13 +10,15 @@ public class Cs_SystemManager : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        Nametag_Left = GameObject.Find("Nametag_Left");
-        Nametag_Right = GameObject.Find("Nametag_Right");
+        // Nametag_Left = GameObject.Find("Nametag_Left");
+        // Nametag_Right = GameObject.Find("Nametag_Right");
+
+        // Nametag_Left.GetComponent<Text>().text = "HH";
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+	    
 	}
 }
