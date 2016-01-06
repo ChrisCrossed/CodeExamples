1a0e72695fcb3d38947009b857d5f096dea0971b
Chris Christensen <unclesam098@gmail.com>
2016-01-05 18:43:19 -0800

    TournamentCaster - Pushing minor changes

---
+++ TournamentCaster/TournamentCaster/Assets/Cs_SystemManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cs_SystemManager : MonoBehaviour
+{
+    GameObject Nametag_Left;
+    GameObject Nametag_Right;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Nametag_Left = GameObject.Find("Nametag_Left");
+        Nametag_Right = GameObject.Find("Nametag_Right");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
