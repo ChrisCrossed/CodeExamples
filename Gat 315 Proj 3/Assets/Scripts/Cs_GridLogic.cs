815c7e0253e05b4ca74239351467a8fbaa93aa5e
c.christensen@digipen.edu <c.christensen@digipen.edu>
2016-04-16 20:30:55 -0700

    Working on individual gameobject creation

--- Gat 315 Proj 3/Assets/Scripts/Cs_GridLogic.cs
+++ Gat 315 Proj 3/Assets/Scripts/Cs_GridLogic.cs
@@ -3,20 +3,34 @@ using System.Collections;
 
 public class Cs_GridLogic : MonoBehaviour
 {
-    Object[] gridList = new Object[25];
+    int sizeOfGrid = 5;
+    Object[] gridList;
 
 	// Use this for initialization
 	void Start ()
     {
+        gridList = new GameObject[sizeOfGrid * sizeOfGrid];
+
         PreloadGridObjects();
 	}
 
     void PreloadGridObjects()
     {
-        Vector3 pos = new Vector3(0, 0, 0);
-        Quaternion quat = new Quaternion();
+        for(int y = 0; y < sizeOfGrid; ++y)
+        {
+            for(int x = 0; x < sizeOfGrid; ++x)
+            {
+                Vector3 pos = new Vector3(x - (sizeOfGrid / 2), 0, y - (sizeOfGrid / 2));
+                Quaternion quat = new Quaternion();
 
-        gridList[0] = Instantiate(Resources.Load("GridObject", typeof(GameObject)), pos, quat) as GameObject;
+                gridList[(y * sizeOfGrid) + x] = Instantiate(Resources.Load("GridObject", typeof(GameObject)), pos, quat) as GameObject;
+
+                if(y < sizeOfGrid / 2)
+                {
+                    // gridList[(y * sizeOfGrid) + y]
+                }
+            }
+        }
     }
 	
 	// Update is called once per frame
