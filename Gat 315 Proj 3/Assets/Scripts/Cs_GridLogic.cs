4b6f1301354e1cb81024260a808c56f1127982dc
c.christensen@digipen.edu <c.christensen@digipen.edu>
2016-04-20 13:47:45 -0700

    Manually created the array.

--- Gat 315 Proj 3/Assets/Scripts/Cs_GridLogic.cs
+++ Gat 315 Proj 3/Assets/Scripts/Cs_GridLogic.cs
@@ -3,31 +3,31 @@ using System.Collections;
 
 public class Cs_GridLogic : MonoBehaviour
 {
-    int sizeOfGrid = 5;
+    int i_sizeOfGrid = 9;
     Object[] gridList;
 
     bool b_Test = true;
 
     int i_CurrNumTowers;
-    int i_CurrSideLength = 1;
+    int i_TowerLevel = 3; // This is an int claming the number of towers per row/column.
 
     // Use this for initialization
     void Start()
     {
-        gridList = new GameObject[sizeOfGrid * sizeOfGrid];
+        gridList = new GameObject[i_sizeOfGrid * i_sizeOfGrid];
 
         PreloadGridObjects();
     }
 
     void PreloadGridObjects()
     {
-        int midPoint = sizeOfGrid / 2;
+        int midPoint = i_sizeOfGrid / 2;
 
-        for (int y = 0; y < sizeOfGrid; ++y)
+        for (int y = 0; y < i_sizeOfGrid; ++y)
         {
-            for (int x = 0; x < sizeOfGrid; ++x)
+            for (int x = 0; x < i_sizeOfGrid; ++x)
             {
-                Vector3 pos = new Vector3(x - (sizeOfGrid / 2), 0, y - (sizeOfGrid / 2));
+                Vector3 pos = new Vector3(x - (i_sizeOfGrid / 2), 0, y - (i_sizeOfGrid / 2));
                 Quaternion quat = new Quaternion();
 
                 GameObject temp = Instantiate(Resources.Load("GridObject", typeof(GameObject)), pos, quat) as GameObject;
@@ -42,18 +42,18 @@ public class Cs_GridLogic : MonoBehaviour
                     temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(false);
                 }
 
-                gridList[(y * sizeOfGrid) + x] = temp;
+                gridList[(y * i_sizeOfGrid) + x] = temp;
             }
         }
     }
 
     public void IncrementNumberOfTowers(bool b_Incremented = true)
     {
+        // If the function call claims a tower was added, increment the towers. Otherwise, this is meant to decrement it.
         if (b_Incremented) ++i_CurrNumTowers; else --i_CurrNumTowers;
 
-        print("Num Towers: " + i_CurrNumTowers + ", length: " + i_CurrSideLength);
-
-        if((float)i_CurrNumTowers / (float)(i_CurrSideLength * i_CurrSideLength) > 0.75f)
+        // i_CurrLevel is a misnomer and represents
+        if((float)i_CurrNumTowers / (float)(i_TowerLevel * i_TowerLevel) > 0.75f)
         {
             IncrementWidthOfTowerArray();
         }
@@ -61,21 +61,360 @@ public class Cs_GridLogic : MonoBehaviour
 
     void IncrementWidthOfTowerArray()
     {
-        // i_CurrSideLength += 2;
-        ++i_CurrSideLength;
+        i_TowerLevel += 2; // Brings the tower level to 5x5, 7x7, etc...
 
-        // Find the X/Y pos of the center
-        int i_Center = sizeOfGrid / 2;
-
-        // yPos = center - i_CurrSideLength
-        int y_ = i_Center - i_CurrSideLength;
-
-        // x runs from center - currsidelength through center + currsidelength.
-        for(int x = y_ - i_CurrSideLength; x < y_ + i_CurrSideLength; ++x)
+        if(i_TowerLevel == 5)
         {
-            var tower = gridList[(y_ * sizeOfGrid) + x] as GameObject;
+            // Top Row
+            int y_ = 2;
 
-            tower.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+            #region Top Row
+            int x_ = 2;
+            GameObject temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 3;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 4;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 5;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 6;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+            #endregion
+
+            // Bot row
+            y_ = 6;
+
+            #region Bot Row
+            x_ = 2;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 3;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 4;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 5;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 6;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+            #endregion
+
+            // Left Wall
+            x_ = 2;
+
+            #region Left Wall
+            y_ = 3;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 4;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 5;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+            #endregion
+
+            x_ = 6;
+
+            #region Right Wall
+            y_ = 3;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 4;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 5;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+            #endregion
+        }
+        else if(i_TowerLevel == 7)
+        {
+            // Top Row
+            int y_ = 1;
+
+            #region Top Row
+            int x_ = 1;
+            GameObject temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 2;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 3;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 4;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 5;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 6;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 7;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+            #endregion
+
+            // Bot row
+            y_ = 7;
+
+            #region Bot Row
+            x_ = 1;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 2;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 3;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 4;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 5;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 6;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 7;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+            #endregion
+
+            // Left Wall
+            x_ = 1;
+
+            #region Left Wall
+            y_ = 2;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 3;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 4;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 5;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 6;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+            #endregion
+
+            // Right Wall
+            x_ = 7;
+
+            #region Right Wall
+            y_ = 2;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 3;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 4;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 5;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 6;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+            #endregion
+        }
+        else if (i_TowerLevel == 9)
+        {
+            // Top Row
+            int y_ = 0;
+
+            #region Top Row
+            int x_ = 0;
+            GameObject temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 1;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 2;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 3;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 4;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 5;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 6;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 7;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 8;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+            #endregion
+
+            // Bot row
+            y_ = 8;
+
+            #region Bot Row
+            x_ = 0;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 1;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 2;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 3;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 4;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 5;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 6;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 7;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            x_ = 8;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+            #endregion
+
+            // Left Wall
+            x_ = 0;
+
+            #region Left Wall
+            y_ = 1;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 2;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 3;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 4;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 5;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 6;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 7;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+            #endregion
+
+            // Right Wall
+            x_ = 8;
+
+            #region Right Wall
+            y_ = 1;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 2;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 3;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 4;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 5;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 6;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+
+            y_ = 7;
+            temp = gridList[(y_ * i_sizeOfGrid) + x_] as GameObject;
+            temp.GetComponent<Cs_GridObjectLogic>().SetGridObjectState(true);
+            #endregion
         }
     }
 
