7b48937abb7aebaf51e98e69360626269ecca118
Chris Christensen <unclesam098@gmail.com>
2016-11-05 12:09:18 -0700

    GAM400_Streak2 - Cascading Tiles
    - Works

---
+++ School/GAM400_Streak2/Assets/Scripts/Cs_BoardDisplay.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Cs_BoardDisplay : MonoBehaviour
+{
+    GameObject go_GridBlock;
+    GameObject go_GridWall;
+
+    int i_Height;
+    int i_Width;
+
+    bool b_IsDone;
+
+	// Use this for initialization
+	void Start ()
+    {
+        go_GridBlock = Resources.Load("GridBlock")      as GameObject;
+        go_GridWall  = Resources.Load("GridBlock_Edge") as GameObject;
+
+        Init_Board(10, 5);
+	}
+
+    // Create a row of Grid objects
+    List<List<GameObject>>  Grid_Columns = new List<List<GameObject>>();
+    List<GameObject>        Grid_Row = new List<GameObject>();
+    // When called from BoardLogic, create the grid background
+    void Init_Board( int i_Width_, int i_Height_, int i_MidWall_ = -1 )
+    {
+        i_Height = i_Height_;
+        i_Width = i_Width_;
+        
+        if(i_MidWall_ > 1 && i_MidWall_ < i_Width_ - 2)
+        {
+
+        }
+        else // No midwall
+        {
+            for(int y_ = 0; y_ < i_Height; ++y_)
+            {
+                GameObject go_WallTemp;
+
+                Grid_Row = new List<GameObject>();
+
+                for(int x_ = 0; x_ < i_Width_; ++x_)
+                {
+                    // Add a GridWall
+                    if( x_ == 0 || x_ == i_Width_ - 1)
+                    {
+                        // Instantiate the object
+                        go_WallTemp = Instantiate(go_GridWall);
+                    }
+                    else
+                    {
+                        // Instantiate the object
+                        go_WallTemp = Instantiate(go_GridBlock);
+                    }
+                    Grid_Row.Add(go_WallTemp);
+                
+                    // Set position
+                    go_WallTemp.transform.position = new Vector3(x_ * go_GridBlock.transform.lossyScale.x, y_ * go_GridBlock.transform.lossyScale.y, 0);
+                    go_WallTemp.transform.parent = GameObject.Find("GridBlockList").transform;
+                }
+                // Add the current Row to the Column List
+                Grid_Columns.Add(Grid_Row);
+            }
+
+            // Reset the Row
+            Grid_Row = new List<GameObject>();
+            // Set the position
+        }
+    }
+
+    // Update is called once per frame
+    int i_X;
+    int i_Y;
+    int i_CurrMax;
+    float f_InitBoardTimer;
+    float f_InitBoardTimer_Max = 0.1f;
+	void Update ()
+    {
+        if(!b_IsDone)
+        {
+            CascadeTiles();
+        }
+    }
+
+    void CascadeTiles()
+    {
+        f_InitBoardTimer += Time.deltaTime;
+
+        if (f_InitBoardTimer >= f_InitBoardTimer_Max)
+        {
+            // Reset Timer
+            f_InitBoardTimer = 0.0f;
+
+            // Set new amounts
+            // Cap X
+            i_X = i_CurrMax;
+            if (i_X > i_Width - 1) i_X = i_Width - 1;
+
+            // Cap Y
+            i_Y = i_CurrMax - i_X;
+            if (i_Y > i_Height - 1) i_Y = i_Height - 1;
+
+            Grid_Columns[i_Y][i_X].GetComponent<Cs_GridBlockLogic>().Set_FadeState(Enum_FadeState.FadeIn);
+
+            // Run through incremental loop
+            for (int i_ = 0; i_ < i_CurrMax; ++i_)
+            {
+                // As X decreases, Y increases.
+                --i_X;
+                i_Y = i_CurrMax - i_X;
+
+                if (i_X < 0) i_X = 0;
+                if (i_Y > i_Height - 1) i_Y = i_Height - 1;
+
+                if (i_Y < i_Height && i_X < i_Width)
+                {
+                    Grid_Columns[i_Y][i_X].GetComponent<Cs_GridBlockLogic>().Set_FadeState(Enum_FadeState.FadeIn);
+                }
+            }
+
+            if (i_CurrMax < i_Height + i_Width)
+            {
+                ++i_CurrMax;
+                print(i_CurrMax);
+            }
+            else
+            {
+                b_IsDone = true;
+            }
+        }
+    }
+}
---
+++ School/GAM400_Streak2/Assets/Scripts/Cs_GridBlockLogic.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Enum_FadeState
+{
+    FadeIn,
+    Stay,
+    FadeOut
+}
+
+public class Cs_GridBlockLogic : MonoBehaviour
+{
+    float f_FadeTimer = 0.0f;
+    float f_FadeTimer_Max = 0.5f;
+    float f_FadePercent = 0.0f;
+    
+    Enum_FadeState e_FadeState;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Color clr_CurrMat = gameObject.GetComponent<MeshRenderer>().material.color;
+        clr_CurrMat.a = 0;
+        gameObject.GetComponent<MeshRenderer>().material.color = clr_CurrMat;
+
+        Set_FadeState(Enum_FadeState.Stay);
+    }
+
+    public void Set_FadeState( Enum_FadeState e_FadeState_ )
+    {
+        e_FadeState = e_FadeState_;
+    }
+
+    void UpdateFadeTimers()
+    {
+        if (e_FadeState == Enum_FadeState.FadeIn)
+        {
+            // Increment Fade Timer
+            if (f_FadeTimer < f_FadeTimer_Max)
+            {
+                f_FadeTimer += Time.deltaTime;
+
+                if (f_FadeTimer > f_FadeTimer_Max) f_FadeTimer = f_FadeTimer_Max;
+            }
+
+            // Set Fade Percent
+            f_FadePercent = f_FadeTimer / f_FadeTimer_Max;
+
+            if (f_FadePercent == 1.0f)
+            {
+                e_FadeState = Enum_FadeState.Stay;
+            }
+        }
+        else if (e_FadeState == Enum_FadeState.FadeOut)
+        {
+            // Decrement Fade Timer
+            if (f_FadeTimer > 0)
+            {
+                f_FadeTimer -= Time.deltaTime;
+
+                if (f_FadeTimer < 0) f_FadeTimer = 0;
+            }
+
+            // Set Fade Percent
+            f_FadePercent = f_FadeTimer / f_FadeTimer_Max;
+
+            if (f_FadePercent == 0f) e_FadeState = Enum_FadeState.Stay;
+        }
+    }
+
+    void UpdateBlockAlpha()
+    {
+        if(e_FadeState == Enum_FadeState.FadeIn || e_FadeState == Enum_FadeState.FadeOut)
+        {
+            Color clr_CurrMat = gameObject.GetComponent<MeshRenderer>().material.color;
+            clr_CurrMat.a = f_FadePercent;
+            gameObject.GetComponent<MeshRenderer>().material.color = clr_CurrMat;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdateBlockAlpha();
+
+        UpdateFadeTimers();
+	}
+}
