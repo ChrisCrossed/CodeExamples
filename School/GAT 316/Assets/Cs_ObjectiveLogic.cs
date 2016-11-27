a0c7fa9947d617a40dceaf579e59296ed5b72903
Chris Christensen <unclesam098@gmail.com>
2016-11-27 00:15:20 -0800

    GAT316_GameNameHere - Began work on Briefcase Objective

---
+++ School/GAT 316/Assets/Cs_ObjectiveLogic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cs_ObjectiveLogic : MonoBehaviour
+{
+    GameObject mdl_Briefcase;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mdl_Briefcase = transform.Find("mdl_Briefcase").gameObject;
+	}
+
+    void OnTriggerEnter( Collider collider_ )
+    {
+        if (collider_.transform.root.gameObject.name == "Player")
+        {
+            mdl_Briefcase.GetComponent<Cs_BriefcaseLogic>().Set_PickedUp();
+        }
+    }
+}
