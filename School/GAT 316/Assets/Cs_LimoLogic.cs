5ac8459292a05dcaf0efb69b4033c9864624cb72
Chris Christensen <unclesam098@gmail.com>
2016-12-01 20:41:50 -0800

    GAT316_GameNameHere - Working on Game Ending
    - Cars drive from left to right on road
    - Limo for end sequence works

---
+++ School/GAT 316/Assets/Cs_LimoLogic.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cs_LimoLogic : MonoBehaviour
+{
+    GameObject go_LimoStop;
+    GameObject go_LimoGoal;
+    NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        go_LimoGoal = GameObject.Find("CarGoal_2");
+        go_LimoStop = GameObject.Find("LimoStop");
+
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+        navMeshAgent.speed = 15f;
+
+        gameObject.transform.position = GameObject.Find("CarStart_2").transform.position;
+
+        Set_LimoStop();
+	}
+
+    void Set_LimoStop()
+    {
+        navMeshAgent.SetDestination(go_LimoStop.transform.position);
+    }
+
+    public void Set_LimoGoal()
+    {
+        navMeshAgent.SetDestination(go_LimoGoal.transform.position);
+    }
+}
