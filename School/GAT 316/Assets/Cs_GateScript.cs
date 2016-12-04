a7f5688a640b472a44796f88203bf0e8796e29ae
Chris Christensen <unclesam098@gmail.com>
2016-12-03 21:54:19 -0800

    GAT316_CarbonGear - Audio semi-fixed

--- School/GAT 316/Assets/Cs_GateScript.cs
+++ School/GAT 316/Assets/Cs_GateScript.cs
@@ -11,37 +11,30 @@ enum Enum_GateDirection
 
 public class Cs_GateScript : MonoBehaviour
 {
-    [SerializeField] Enum_GateDirection e_GateDirection = Enum_GateDirection.Right;
+    [SerializeField]
+    Enum_GateDirection e_GateDirection = Enum_GateDirection.Right;
 
-    [SerializeField] float f_MoveTime_Max = 1.0f;
+    [SerializeField]
+    float f_MoveTime_Max = 1.0f;
     float f_MoveTime;
 
-    [SerializeField] bool b_StartOpen;
-    [SerializeField] bool b_IsObjectiveActive = true;
+    [SerializeField]
+    bool b_StartOpen;
+    [SerializeField]
+    bool b_IsObjectiveActive = true;
 
     Vector3 v3_FinalPos;
     Vector3 v3_StartPos;
 
     bool b_IsOpen;
 
-    AudioSource as_AudioSource;
-    AudioClip sfx_GarageOpen;
-    float f_EndClip;
-    AudioClip sfx_GarageSlam;
-    bool b_FirstFramePassed;
+    bool b_FirstFrame;
 
     // Use this for initialization
     void Start()
     {
         // Set start position
         v3_StartPos = gameObject.transform.position;
-
-        b_PreviousState = !b_StartOpen;
-
-        as_AudioSource = gameObject.GetComponent<AudioSource>();
-        sfx_GarageOpen = Resources.Load("SFX_GarageOpen") as AudioClip;
-        f_EndClip = 8.25f;
-        // sfx_GarageSlam = Resources.Load("SFX_GarageOpen") as AudioClip;
 
         // Set final position
         if (e_GateDirection == Enum_GateDirection.Right)
@@ -61,103 +54,116 @@ public class Cs_GateScript : MonoBehaviour
             v3_FinalPos = gameObject.transform.position + (-gameObject.transform.up * (gameObject.transform.lossyScale.y + 0.1f));
         }
 
-        if (b_StartOpen)
-        {
-            b_IsOpen = true;
-            Set_DoorOpen(b_IsOpen);
-        }
-        else
-        {
-            b_PreviousState = true;
-        }
+        as_AudioSource = gameObject.GetComponent<AudioSource>();
+        sfx_GarageOpen = Resources.Load("SFX_GarageOpen") as AudioClip;
 
+        if (b_StartOpen) Set_DoorOpen(!b_IsOpen);
     }
 
     // Update is called once per frame
-	void Update ()
+    bool b_PreviousDoorState;
+    void Update()
     {
         PositionDoor();
-	}
-    
-    void PositionDoor()
+        b_FirstFrame = true;
+    }
+
+    bool b_PreviousSoundState;
+    AudioSource as_AudioSource;
+    AudioClip sfx_GarageOpen;
+    float f_EndClip = 8.25f;
+    void PlaySFX()
     {
-        // Lerp between the door's current position and where it is going
-        // If we're opening the door...
-        if (b_IsOpen)
+        if (b_IsOpen != b_PreviousSoundState)
         {
-            print("Got here: " + gameObject.name);
-            // Increment timer
-            f_MoveTime += Time.deltaTime;
-
-            // Clamp
-            if (f_MoveTime > f_MoveTime_Max)
+            // Play SFX
+            if(as_AudioSource.isPlaying)
             {
-                f_MoveTime = f_MoveTime_Max;
-
-                if(f_MoveTime == f_MoveTime_Max)
-                {
-                    as_AudioSource.time = f_EndClip;
-                }
+                as_AudioSource.Stop();
             }
+            as_AudioSource.loop = false;
+            as_AudioSource.clip = sfx_GarageOpen;
+            as_AudioSource.Play();
 
-            // Lerp position
-            Vector3 v3_LerpPosition = Vector3.Lerp(v3_StartPos, v3_FinalPos, f_MoveTime / f_MoveTime_Max);
-                
-            // Set position
-            gameObject.transform.position = v3_LerpPosition;
-
-            // Set the new state
-            // if (f_MoveTime == f_MoveTime_Max) b_PreviousState = b_IsOpen;
-            // b_PreviousDoorState = !b_IsOpen;
-        }
-        else // Closing door
-        {
-            // Decrement timer. Clamp
-            if (f_MoveTime > 0)
-            {
-                f_MoveTime -= Time.deltaTime * 3;
-
-                if (f_MoveTime < 0)
-                {
-                    f_MoveTime = 0;
-                    as_AudioSource.time = f_EndClip;
-                }
-            }
-
-            // Lerp percentage
-            float f_LerpPercent = (f_MoveTime_Max - f_MoveTime) / f_MoveTime_Max;
-
-            // Lerp position (from 0% to 100%)
-            Vector3 v3_LerpPosition = Vector3.Lerp(v3_FinalPos, v3_StartPos, f_LerpPercent);
-
-            // Set position
-            gameObject.transform.position = v3_LerpPosition;
+            b_PreviousSoundState = b_IsOpen;
         }
     }
 
-    bool b_PreviousState;
-    public void Set_DoorOpen( bool b_IsOpen_ )
+    void PositionDoor()
     {
-        if(b_IsOpen_ != b_PreviousState)
+        // Lerp between the door's current position and where it is going
+        if (b_IsOpen != b_PreviousDoorState)
         {
-            if(b_IsObjectiveActive)
+            // If we're opening the door...
+            if (b_IsOpen)
             {
-                b_IsOpen = b_IsOpen_;
-            }
+                // Increment timer
+                f_MoveTime += Time.deltaTime;
 
-            if(b_FirstFramePassed)
+                // Clamp
+                if (f_MoveTime > f_MoveTime_Max) f_MoveTime = f_MoveTime_Max;
+
+                // Lerp position
+                Vector3 v3_LerpPosition = Vector3.Lerp(v3_StartPos, v3_FinalPos, f_MoveTime / f_MoveTime_Max);
+
+                // Set position
+                gameObject.transform.position = v3_LerpPosition;
+
+                // Set the new state
+                if (f_MoveTime == f_MoveTime_Max)
+                {
+                    // as_AudioSource.time = f_EndClip;
+
+                    as_AudioSource.Stop();
+
+                    // b_PreviousDoorState = !b_PreviousDoorState;
+                }
+            }
+            else // Closing door
             {
-                // Play SFX
-                as_AudioSource.Stop();
-                as_AudioSource.loop = false;
-                as_AudioSource.clip = sfx_GarageOpen;
-                as_AudioSource.Play();
-            }
+                // Decrement timer
+                // If an objective door
+                f_MoveTime -= Time.deltaTime * 3;
 
-            b_PreviousState = !b_IsOpen_;
+                // Clamp
+                if (f_MoveTime < 0) f_MoveTime = 0;
+
+                // Lerp percentage
+                float f_LerpPercent = (f_MoveTime_Max - f_MoveTime) / f_MoveTime_Max;
+
+                // Lerp position (from 0% to 100%)
+                Vector3 v3_LerpPosition = Vector3.Lerp(v3_FinalPos, v3_StartPos, f_LerpPercent);
+
+                // Set position
+                gameObject.transform.position = v3_LerpPosition;
+
+                // Set the new state
+                if (f_MoveTime == 0)
+                {
+                    // as_AudioSource.time = f_EndClip;
+                    as_AudioSource.Stop();
+
+                    // b_PreviousDoorState = !b_PreviousDoorState;
+                }
+
+            }
         }
-        
-        b_FirstFramePassed = true;
+    }
+
+    public void Set_DoorOpen(bool b_IsOpen_)
+    {
+        if (b_IsObjectiveActive)
+        {
+            b_IsOpen = b_IsOpen_;
+            b_PreviousDoorState = !b_IsOpen_;
+
+            // b_PreviousSoundState = !b_IsOpen;
+        }
+
+        if(b_FirstFrame)
+        {
+            PlaySFX();
+        }
     }
 
     public bool Get_DoorOpen()
@@ -165,7 +171,7 @@ public class Cs_GateScript : MonoBehaviour
         return b_IsOpen;
     }
 
-    public void Set_ObjectiveActive( bool b_IsActive_ )
+    public void Set_ObjectiveActive(bool b_IsActive_)
     {
         b_IsObjectiveActive = b_IsActive_;
     }
